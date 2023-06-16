610c7736d1fd67ba750d8c0d76367104ab25438e
Raj <91188743+FahadKhanRaj@users.noreply.github.com>
2023-06-16 17:14:35 +0600

    Add files via upload

---
+++ admin/partials/footer.php
@@ -0,0 +1,10 @@
+<!-- Footer Section Starts -->
+<div class="footer">
+            <div class="wrapper">
+                <p class="text-center">All rights reserved. <a href="#"> Food Chemistry </a></p>
+            </div>
+        </div>
+        <!-- Footer Section Ends -->
+
+    </body>
+</html>
